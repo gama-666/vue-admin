e9ac7bbafcee03c71602a007e1b40acdbd2d8b3d
cyd <245692559@qq.com>
2020-09-16 17:55:55 +0800

    信息分类静态页，编辑、删除、批量删除的弹框方法

---
+++ vue-admin/src/views/Info/dialog/popup2.0.vue
@@ -0,0 +1,93 @@
+<template>
+  <el-dialog title="新增" :visible.sync="dialog_info_flag" @close="close" width="580px">
+    <el-form :model="form">
+      <el-form-item label="标题：" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="请选择活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="类型：" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="概况：" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
+</template>
+<script>
+import { ref } from "@vue/composition-api";
+export default {
+  name: "popup",
+  data() {
+    return {
+      dialog_info_flag: true,
+      gridData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        }
+      ],
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      },
+      formLabelWidth: "70px"
+    };
+  },
+  methods: {
+    //回调
+    close() {
+      this.dialog_info_flag = false;
+      this.$emit("close", false);
+    }
+  },
+  //单向数据流，父级->子级,不能反向修改
+  props: {
+    flag: {
+      type: Boolean,
+      default: false
+    }
+  },
+  //通过watch来监听,修改值
+  watch: {
+    flag: {
+      //新值newValue，旧值oldValue
+      handler(newValue, oldValue) {
+        this.dialog_info_flag = newValue;
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
--- vue-admin/src/views/Info/list.vue
+++ vue-admin/src/views/Info/list.vue
@@ -1,27 +1,159 @@
 <template>
-  <div id="infolist">
-    <el-row :gutter="20">
-      <el-col :span="8">
-        <div class="grid-content bg-purple">11</div>
-      </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple">22</div>
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content bg-purple">33</div>
-      </el-col>
-      <el-col :span="4">
-        <div class="grid-content bg-purple">44</div>
-      </el-col>
-    </el-row>
+  <div id="wrap">
+    <el-button type="danger" size="small">添加一级分类</el-button>
+    <div class="line"></div>
+    <div>
+      <el-row :gutter="30">
+        <el-col :span="8">
+          <div class="list-wrap">
+            <div class="infoList">
+              <h4>
+                <svg-icon iconName="plus" />新闻
+                <div class="button-group">
+                  <el-button size="small" type="danger" round>编辑</el-button>
+                  <el-button size="small" type="success" round>添加子级</el-button>
+                  <el-button size="small" round>删除</el-button>
+                </div>
+              </h4>
+              <ul>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="small" type="danger" round>编辑</el-button>
+                    <el-button size="small" round>删除</el-button>
+                  </div>
+                </li>
+                <li>国际</li>
+                <li>行业</li>
+                <li>航空</li>
+                <li>军事</li>
+              </ul>
+            </div>
+            <div class="infoList">
+              <h4>
+                <svg-icon iconName="reduce" />新闻
+              </h4>
+              <ul>
+                <li>国内</li>
+                <li>国际</li>
+                <li>行业</li>
+                <li>航空</li>
+                <li>军事</li>
+              </ul>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="16">
+          <div class="menu-title">
+            <h4>一级分类编辑</h4>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "infolist",
-  setup(props, context) {}
+  setup(props) {}
 };
 </script>
 <style lang="scss" scoped>
+#wrap {
+  .line {
+    width: 100%;
+    height: 1px;
+    background-color: #dedede;
+    margin: 28px 0px;
+  }
+}
+.list-wrap {
+  div:first-child {
+    &::before {
+      top: 20px;
+    }
+  }
+  div:last-child {
+    &::before {
+      bottom: 23px;
+    }
+  }
+}
 
+.menu-title {
+  line-height: 44px;
+  padding-left: 22px;
+  background-color: #f3f3f3;
+  h4 {
+    margin: 0px;
+  }
+}
+
+.infoList {
+  cursor: pointer;
+  line-height: 44px;
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    left: 22px;
+    top: 0px;
+    bottom: 0px;
+    width: 32px;
+    border-left: 1px dotted #000000;
+    z-index: 1;
+  }
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+  h4 {
+    padding-left: 40px;
+    margin: 0px;
+    position: relative;
+  }
+  svg {
+    position: absolute;
+    left: 15px;
+    top: 15px;
+    background-color: #ffffff;
+    font-size: 14px;
+    z-index: 2;
+  }
+  li {
+    position: relative;
+    padding-left: 36px;
+    margin-left: 24px;
+    &::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 20px;
+      width: 32px;
+      border-bottom: 1px dotted #000000;
+    }
+  }
+  .button-group {
+    display: none;
+    position: absolute;
+    z-index: 2;
+    right: 11px;
+    top: 0px;
+    .el-button {
+      margin-left: 0px !important;
+      margin: 5px 10px 5px 0px;
+      font-size: 13px;
+    }
+  }
+  h4,
+  li {
+    &:hover {
+      @include webkit(transition, all 0.5s ease 0s);
+      background-color: #f3f3f3;
+      .button-group {
+        display: block;
+      }
+    }
+  }
+}
 </style>
